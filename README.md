adc57ab512f185c45249c974068df295d1b9c261
Justin Kuta <jpkuta@gmail.com>
2024-02-18 14:20:19 -0600

    top active user method, readme updates

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## Description
 This python script aggrigates user timestamp data from a input.json file.
 Additionally there is a method which outputs how many times each command was executed by a user. This aids in identifying the most frequent commands utilized by each user.
+Finally there is a method which returns the 3 top active users with the number of commands which they ran. This data can be used to identify the most active users in the system.
 
 
 ## How to use
@@ -9,4 +10,4 @@ Additionally there is a method which outputs how many times each command was exe
 1. download script
 2. place 'input.json' input file in the same directory as buildUserEventDictionary.py script
 3. execute 'python buildUserEventDictionary.py'
-4. See 'output.py' output file in the excuted directory
+4. See 'output.py', 'frequency_output.json' and 'top_3_active_users_output.json' output files in the excuted directory
--- buildUserEventDictionary.py
+++ buildUserEventDictionary.py
@@ -8,6 +8,8 @@ with open('input.json', 'r') as f:
 aggregated_timestamp_data = {}
 # Create dictionary to aggregate command frequency data
 aggregated_freq_data = {}
+# Create dictionary to aggregate user activity data
+user_activity = {}
 
 # Iterate over events and aggregate timestamp data for user events
 for event in data:
@@ -35,10 +37,29 @@ for event in data:
     else:
         aggregated_freq_data[user][command] += 1
 
+# Iterate over events and aggregate user activity data
+for event in data:
+    user = event['user']
+    
+    if user not in user_activity:
+        user_activity[user] = 1
+    else:
+        user_activity[user] += 1
+
+# Sort the users based on activity
+sorted_users = sorted(user_activity.items(), key=lambda x: x[1], reverse=True)
+
+# Get the top 3 most active users
+top_users = sorted_users[:3]
+
 # Write aggregated timestamp data to output.json file
 with open('output.json', 'w') as f:
     json.dump(aggregated_timestamp_data, f, indent=4)
 
-# Write aggregated frequency data to output JSON file
+# Write aggregated frequency data to frequency_output.json file
 with open('frequency_output.json', 'w') as f:
     json.dump(aggregated_freq_data, f, indent=4)
+
+# Write top users to output top_3_active_users_output.json file
+with open('top_3_active_users_output.json', 'w') as f:
+    json.dump(top_users, f, indent=4)
